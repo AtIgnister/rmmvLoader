fd6ddb85af5665786d8ca6994aed2335a42610e9
Tim <137562253+AtIgnister@users.noreply.github.com>
2024-03-29 17:15:57 +0100

    update auto adding vendors

--- rmmvLoader/cli/commands/add.js
+++ rmmvLoader/cli/commands/add.js
@@ -2,17 +2,37 @@ const path = "./include.json";
 
 async function add(contents, name) {
     // { name: "my-package" }
+    name = name.split("/")
 
     let vendors
     vendors = contents.vendors
-
-    vendors[0].plugins.push(name)
+    vendors = addVendor(vendors, name)
 
     contents = {
         "vendors": vendors
     }
 
     await Bun.write(path, JSON.stringify(contents));
+}
+
+function addVendor(vendors, name) {
+    let i = 0
+    vendors.forEach(vendor => {
+        if (vendor.vendor === name[0]) {
+           vendor.plugins.push(name[1])
+           return vendors
+        }
+    });
+
+    vendors.push(
+        {
+            vendor: name[0],
+            plugins: [name[1]]
+        }
+    )
+
+    return vendors
+
 }
 
 module.exports = add
